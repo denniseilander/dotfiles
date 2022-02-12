87b793708229e246be2452d9978065d26e73b78c
Dennis <eilander.dennis@gmail.com>
2022-02-12 18:12:57 +0100

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -2,11 +2,6 @@
 
 echo "Setting up your Mac..."
 
-# Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
-fi
-
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -15,9 +10,34 @@ if test ! $(which brew); then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
+# Oh My Zsh Setup
+ZSH_CUSTOM_PATH=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+# Check for Oh My Zsh and install if we don't have it
+if [ ! -e $HOME/.oh-my-zsh ]; then
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+if [ ! -e $ZSH_CUSTOM_PATH/plugins/zsh-autosuggestions ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM_PATH/plugins/zsh-autosuggestions
+fi
+
+# Install spaceship prompt for ZSH
+echo $ZSH_CUSTOM_PATH/themes/spaceship-prompt/spaceship.zsh-theme
+if [ ! -e $ZSH_CUSTOM_PATH/themes/spaceship-prompt ]; then
+  echo 'cloning spaceship prompt'
+  git clone https://github.com/spaceship-prompt/spaceship-prompt.git $ZSH_CUSTOM_PATH/themes/spaceship-prompt --depth=1
+
+  rm -rf $ZSH_CUSTOM_PATH/themes/spaceship.zsh-theme
+  ln -s $ZSH_CUSTOM_PATH/themes/spaceship-prompt/spaceship.zsh-theme $ZSH_CUSTOM_PATH/themes/spaceship.zsh-theme
+fi
+
+# Create hushlogin file to remove "last login" in terminal
+touch ~/.hushlogin
 
 # Update Homebrew recipes
 brew update
@@ -28,19 +48,18 @@ brew bundle --file $DOTFILES/Brewfile
 
 # Set default MySQL root password and auth type
 brew services start mysql
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY ''; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
 pecl install pcov igbinary imagick redis
 
-# Symlink composer to directory in $PATH
-sudo ln -s /opt/homebrew/bin/composer /usr/local/bin/composer
-
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose themsaid/ibis
+/opt/homebrew/bin/composer global require laravel/installer laravel/valet beyondcode/expose themsaid/ibis
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
+
+echo 'To support multiple PHP versions at once, check https://dev.to/dyriavin/serving-sites-with-different-php-version-using-laravel-valet-49jl'
 
 # Create a Sites directory
 mkdir $HOME/Sites
@@ -51,11 +70,14 @@ mkdir $HOME/Sites/packages
 # Clone Github repositories
 $DOTFILES/clone.sh
 
+# Symlink the .gitignore_globale file to the home directory
+ln -s $DOTFILES/.gitignore_global $HOME/.gitignore_global
+
+# Set the global gitignore file
+git config --global core.excludesfile ~/.gitignore_global
+
 # Symlink the Mackup config file to the home directory
 ln -s $DOTFILES/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences - we will run this last because this will reload the shell
 source $DOTFILES/.macos
-
-# Create hushlogin file to remove "last login" in terminal
-touch ~/.hushlogin
